4dd97efac97aee5bcfc55de2434b113f3d4b8b79
oh-seung <112290409+oh-seung@users.noreply.github.com>
2022-09-01 16:17:38 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,13 +2,11 @@ import setuptools
 
 setuptools.setup(name='oh-seung',
 		version='0.0.1',
-		description='Standard Dictionary Management(Korean Recommand)',
+		description='Standard Dictionary Management(Korean Ver)',
 		author='oh seung',
 		author_email='seung.oh17@gmail.com',
-		url='',
-		license='BSD-2',
-		python_requires='>=3',
+		url='https://github.com/oh-seung/pystddic',
+		python_requires='>=3.8',
 		include_package_data=True,
 		packages = setuptools.find_packages(),
-		zip_safe=False
     )
